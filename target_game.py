63336548de9bd264a7c3b785c04cdc3e9f5f529d
Zahar Kohut <zaharentv2005@gmail.com>
2022-11-04 12:05:58 +0200

    a

---
+++ target_game.py
@@ -0,0 +1,40 @@
+from typing import List
+
+def generate_grid() -> List[List[str]]:
+    """
+    Generates list of lists of letters - i.e. grid for the game.
+    e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
+    """
+    pass
+
+
+def get_words(f: str, letters: List[str]) -> List[str]:
+    """
+    Reads the file f. Checks the words with rules and returns a list of words.
+    """
+    pass
+
+
+
+def get_user_words() -> List[str]:
+    """
+    Gets words from user input and returns a list with these words.
+    Usage: enter a word or press ctrl+d to finish for *nix or Ctrl-Z+Enter 
+    for Windows.
+    Note: the user presses the enter key after entering each word.
+    """
+    pass
+
+
+def get_pure_user_words(user_words: List[str], letters: List[str], words_from_dict: List[str]) -> List[str]:
+    """
+    (list, list, list) -> list
+
+    Checks user words with the rules and returns list of those words
+    that are not in dictionary.
+    """
+    pass
+
+
+def results():
+    pass
