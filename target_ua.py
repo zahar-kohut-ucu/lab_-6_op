e39ce9c402f479f4091550d37688f3953d7bef00
Zahar Kohut <zaharentv2005@gmail.com>
2022-11-06 20:59:49 +0200

    get_words() prolly done

--- target_ua.py
+++ target_ua.py
@@ -1,10 +1,11 @@
 '''Target game (Ukrainian)'''
+from typing import List
 import random
 
-def generate_grid():
+def generate_grid() -> List[str]:
     '''
     Generates list of letters - i.e. grid for the game.
-    e.g. ['е', 'п', 'в', 'о', 'є']
+    e.g. ['e', 'п', 'в', 'o', 'є']
     '''
     ukr_alph = list('абвгґдеєжзиіїйклмнопрстуфхцчшщюя')
     board = []
@@ -29,8 +30,29 @@ def get_user_words() -> List[str]:
             break
     return user_words
 
-def get_words(_f, letters):
+def get_words(_f: str, letters: List[str]) -> List[tuple[str]]:
     '''
     Return list of tuples (pairs - (word, part_of_speech)) with length less than 6
     and starting with a letter from 'letters' list.
+    '''
+    parts_of_speech_dict = {'/n': 'noun', 'noun': 'noun', '/numr': None, 'adj': 'adjective', '/adj': 'adjective', \
+    'adv': 'adverb', '/v': 'verb', 'verb': 'verb', 'advp': 'verb'}
+    words = []
+    with open(_f, 'r', encoding='utf-8') as base:
+        content = base.read().split('\n')
+        for word in content:
+            word = word.lower() + ' '
+            if len(word[:word.find(' ')]) <= 5 and word[0] in letters:
+                part = None
+                for item in parts_of_speech_dict.items():
+                    if item[0] in word:
+                        part = item[1]
+                if part:
+                    words.append((word[:word.find(' ')], part))
+    return words
+
+
+def check_user_words(user_words: List[str], language_part: str, letters: List[str], dict_of_words: List[str]) -> List[str]:
+    '''
+    Returns a list of words a user guessed and didn't guess in game of Target.
     '''
