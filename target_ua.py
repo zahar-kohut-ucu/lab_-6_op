558972f0f83e57491c01514a4597a05e6a3a2819
Zahar Kohut <zaharentv2005@gmail.com>
2022-11-06 23:38:47 +0200

    first ready version

--- target_ua.py
+++ target_ua.py
@@ -35,8 +35,8 @@ def get_words(_f: str, letters: List[str]) -> List[tuple[str]]:
     Return list of tuples (pairs - (word, part_of_speech)) with length less than 6
     and starting with a letter from 'letters' list.
     '''
-    parts_of_speech_dict = {'/n': 'noun', 'noun': 'noun', '/numr': None, 'adj': 'adjective', '/adj': 'adjective', \
-    'adv': 'adverb', '/v': 'verb', 'verb': 'verb', 'advp': 'verb'}
+    parts_of_speech_dict = {'/n': 'noun', 'noun': 'noun', '/numr': None, 'adv': 'adverb', 'adj': 'adjective', \
+    '/adj': 'adjective','/v': 'verb', 'verb': 'verb', 'advp': 'verb'}
     words = []
     with open(_f, 'r', encoding='utf-8') as base:
         content = base.read().split('\n')
@@ -52,7 +52,15 @@ def get_words(_f: str, letters: List[str]) -> List[tuple[str]]:
     return words
 
 
-def check_user_words(user_words: List[str], language_part: str, letters: List[str], dict_of_words: List[str]) -> List[str]:
+def check_user_words(user_words: List[str], language_part: str, \
+letters: List[str], dict_of_words: List[str]) -> List[str]:
     '''
     Returns a list of words a user guessed and didn't guess in game of Target.
     '''
+    right_words = []
+    for item in dict_of_words:
+        if item[0][0] in letters and item[1] == language_part:
+            right_words.append(item[0])
+    right_user_words = [el1 for el1 in user_words if el1 in right_words]
+    missed_words = [el2 for el2 in right_words if el2 not in user_words]
+    return (right_user_words, missed_words)
